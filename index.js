ac5cb72918e4ef4490ce781e01364fb605880ab8
Nur Santo <nursanto@gmail.com>
2024-06-15 00:39:46 +0700

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementsByClass("navBar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
